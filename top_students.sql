595897198d9dde0df18252825e1059d684568f35
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-10 02:01:53 +0300

    SQL

---
+++ top_students.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS STUDENT;
+
+CREATE TABLE STUDENT (
+ID INTEGER PRIMARY KEY,
+NAME VARCHAR(100),
+SCORE FLOAT
+);
+
+
+INSERT INTO STUDENT ( ID, NAME, SCORE) VALUES
+(1, 'Bob', 50.5),
+(2, 'John', 65.5),
+(3, 'Harry', 45.0),
+(4, 'Dick', 85.0),
+(5, 'Dev', 25.0),
+(6, 'Sid', 98.0),
+(7, 'Tom', 90.0),
+(8, 'Julia', 70.5),
+(9, 'Erica', 81.0),
+(10, 'Jerry', 85.0);
+
+SELECT ID, NAME
+FROM STUDENT
+ORDER BY SCORE DESC, ID ASC
+LIMIT 3;
+
+
